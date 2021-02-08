dd2217585bb51bfc98b417d7578f338f57e7bfae
daoudi <daoudi.mohamedsalah@gmail.com>
2021-02-08 15:36:16 +0100

    test

--- test/test.spec.ts
+++ test/test.spec.ts
@@ -1,10 +1,20 @@
 import * as assert from "assert"
 import * as chai from "chai"
 const expect = chai.expect
+import { User } from"../src/model/User"
 
-describe("My first test", () => {
+const data = {
+    id: 5, 
+    surname: "Ben",
+    name: "John",
+}
+const user: any = new User(data)
 
-    it("1 is equal to 1", () => {
-        expect(1).to.equal(1)
+describe("Test models", () => {
+
+    it("Test if user model is valid", () => {
+        expect(user.id).to.equal(5)
+        expect(user.name).to.equal("John")
+        expect(user.surname).not.to.equal("Ben")
     })
 })
